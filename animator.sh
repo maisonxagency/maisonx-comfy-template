ecf5d731324707a93799fe59300ce7da97657c08
maisonxagency <moonflicker@proton.me>
2026-03-23 00:41:36 +0400

    Add files via upload

---
+++ animator.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+set -e
+source /venv/main/bin/activate
+
+WORKSPACE=${WORKSPACE:-/workspace}
+COMFYUI_DIR="${WORKSPACE}/ComfyUI"
+
+echo "=== ComfyUI запускает ( x-mode) ==="
+
+APT_PACKAGES=()
+PIP_PACKAGES=()
+
+NODES=(
+    "https://github.com/kijai/ComfyUI-WanVideoWrapper"
+    "https://github.com/chflame163/ComfyUI_LayerStyle"
+    "https://github.com/yolain/ComfyUI-Easy-Use"
+    "https://github.com/kijai/ComfyUI-KJNodes"
+    "https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"
+    "https://github.com/kijai/ComfyUI-segment-anything-2"
+    "https://github.com/cubiq/ComfyUI_essentials"
+    "https://github.com/fq393/ComfyUI-ZMG-Nodes"
+    "https://github.com/kijai/ComfyUI-WanAnimatePreprocess"
+    "https://github.com/rgthree/rgthree-comfy"
+    "https://github.com/jnxmx/ComfyUI_HuggingFace_Downloader"
+    "https://github.com/teskor-hub/NEW-UTILS.git"
+    "https://github.com/teskor-hub/comfyui-teskors-utils.git"
+)
+
+# ТВОИ МОДЕЛИ (УЖЕ С HF maisonx)
+CLIP_MODELS=(
+    "https://huggingface.co/maisonx/wan-models/resolve/main/klip_vision.safetensors"
+)
+
+CLIPS=(
+"https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
+)
+
+TEXT_ENCODERS=(
+"https://huggingface.co/maisonx/wan-models/resolve/main/text_enc.safetensors"
+)
+
+UNET_MODELS=(
+"https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/diffusion_models/z_image_turbo_bf16.safetensors"
+)
+
+VAE_MODELS=(
+"https://huggingface.co/maisonx/wan-models/resolve/main/vae.safetensors"
+)
+
+DETECTION_MODELS=(
+"https://huggingface.co/Wan-AI/Wan2.2-Animate-14B/resolve/main/process_checkpoint/det/yolov10m.onnx"
+"https://huggingface.co/Kijai/vitpose_comfy/resolve/main/onnx/vitpose_h_wholebody_data.bin"
+"https://huggingface.co/Kijai/vitpose_comfy/resolve/main/onnx/vitpose_h_wholebody_model.onnx"
+)
+
+LORAS=(
+"https://huggingface.co/maisonx/wan-models/resolve/main/WanFun.reworked.safetensors"
+"https://huggingface.co/maisonx/wan-models/resolve/main/light.safetensors"
+"https://huggingface.co/maisonx/wan-models/resolve/main/WanPusa.safetensors"
+"https://huggingface.co/maisonx/wan-models/resolve/main/wan.reworked.safetensors"
+)
+
+CLIP_VISION=(
+"https://huggingface.co/maisonx/wan-models/resolve/main/klip_vision.safetensors"
+)
+
+DEFFUSION=(
+"https://huggingface.co/maisonx/wan-models/resolve/main/WanModel.safetensors"
+)
+
+### ─────────────────────────────────────────────
+
+function provisioning_start() {
+    echo ""
+    echo "##############################################"
+    echo "# ебашим жоска и мрачно                      #"
+    echo "##############################################"
+    echo ""
+
+    provisioning_get_apt_packages
+    provisioning_clone_comfyui
+    provisioning_install_base_reqs
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+
+    provisioning_get_files "${COMFYUI_DIR}/models/clip"               "${CLIP_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/clip_vision"        "${CLIP_VISION[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/text_encoders"      "${TEXT_ENCODERS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/vae"                "${VAE_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/diffusion_models"   "${DIFFUSION_MODELS[@]}"
+
+    provisioning_get_files "${COMFYUI_DIR}/models/detection"          "${DETECTION_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/loras"              "${LORAS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/diffusion_models"   "${DEFFUSION[@]}"
+
+    echo ""
+    echo "Starting ComfyUI..."
+    echo ""
+}
+
+function provisioning_clone_comfyui() {
+    if [[ ! -d "${COMFYUI_DIR}" ]]; then
+        git clone https://github.com/comfyanonymous/ComfyUI.git "${COMFYUI_DIR}"
+    fi
+    cd "${COMFYUI_DIR}"
+}
+
+function provisioning_install_base_reqs() {
+    if [[ -f requirements.txt ]]; then
+        pip install --no-cache-dir -r requirements.txt
+    fi
+}
+
+function provisioning_get_apt_packages() {
+    if [[ ${#APT_PACKAGES[@]} -gt 0 ]]; then
+        sudo apt update && sudo apt install -y "${APT_PACKAGES[@]}"
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ ${#PIP_PACKAGES[@]} -gt 0 ]]; then
+        pip install --no-cache-dir "${PIP_PACKAGES[@]}"
+    fi
+}
+
+function provisioning_get_nodes() {
+    mkdir -p "${COMFYUI_DIR}/custom_nodes"
+    cd "${COMFYUI_DIR}/custom_nodes"
+
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="./${dir}"
+
+        if [[ -d "$path" ]]; then
+            (cd "$path" && git pull --ff-only || { git fetch && git reset --hard origin/main; })
+        else
+            git clone "$repo" "$path" --recursive
+        fi
+
+        requirements="${path}/requirements.txt"
+        if [[ -f "$requirements" ]]; then
+            pip install --no-cache-dir -r "$requirements"
+        fi
+    done
+}
+
+function provisioning_get_files() {
+    local dir="$1"
+    shift
+    local files=("$@")
+
+    mkdir -p "$dir"
+
+    for url in "${files[@]}"; do
+        local auth_header=""
+        if [[ -n "$HF_TOKEN" && "$url" =~ huggingface\.co ]]; then
+            auth_header="--header=Authorization: Bearer $HF_TOKEN"
+        fi
+
+        wget $auth_header -nc --content-disposition -P "$dir" "$url"
+    done
+}
+
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
+fi
+
+cd "${COMFYUI_DIR}"
+python main.py --listen 0.0.0.0 --port 8188
